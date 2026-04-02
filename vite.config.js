c2c90e2664aecb23a423721e626389e07f1c2b7c
RxDB <208150718+RxDB@users.noreply.github.com>
2026-04-02 17:08:01 +0530

    vecel fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/intern_signup/',
+  base: '/',
   build: {
     rollupOptions: {
       output: {
